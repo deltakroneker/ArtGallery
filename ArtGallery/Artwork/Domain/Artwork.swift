dc0a37dcf4b44eac9c98763f3141c8f2eca49df7
deltakroneker <nikola.milic.ios@gmail.com>
2023-05-09 13:49:48 +0200

    Add Artwork model

---
+++ ArtGallery/Artwork/Domain/Artwork.swift
@@ -0,0 +1,19 @@
+//
+//  Artwork.swift
+//  ArtGallery
+//
+//  Created by nikolamilic on 5/9/23.
+//
+
+import Foundation
+
+struct Artwork {
+    let title: String
+    let description: String
+    let date: String
+    
+    let physicalMedium: String
+    
+    let principalMaker: String
+    let makerLine: String
+}
