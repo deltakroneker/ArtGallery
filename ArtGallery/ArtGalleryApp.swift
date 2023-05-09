1dff8f6d6807bce57034fd2b7b2d839d00520332
deltakroneker <nikola.milic.ios@gmail.com>
2023-05-09 13:10:52 +0200

    Initial Commit

---
+++ ArtGallery/ArtGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGalleryApp.swift
+//  ArtGallery
+//
+//  Created by nikolamilic on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ArtGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtGalleryUITests/ArtGalleryUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ArtGalleryUITestsLaunchTests.swift
+//  ArtGalleryUITests
+//
+//  Created by nikolamilic on 5/9/23.
+//
+
+import XCTest
+
+final class ArtGalleryUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
