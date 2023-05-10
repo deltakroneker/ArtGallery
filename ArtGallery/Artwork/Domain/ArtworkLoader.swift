7688781eaada52ed84a1217edb3639f6ac42b0a4
deltakroneker <nikola.milic.ios@gmail.com>
2023-05-10 14:38:43 +0200

    Add single artwork object fetching to ArtworkLoader

--- ArtGallery/Artwork/Domain/ArtworkLoader.swift
+++ ArtGallery/Artwork/Domain/ArtworkLoader.swift
@@ -10,4 +10,5 @@ import Combine
 
 protocol ArtworkLoader {
     func loadBriefs(for query: String) async -> AnyPublisher<[ArtworkBrief], Error>
+    func loadArtwork(for id: String) async -> AnyPublisher<Artwork, Error>
 }
