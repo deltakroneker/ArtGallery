54ad2207bfed3ab3c9dc87921459ab9b3e8b5831
deltakroneker <nikola.milic.ios@gmail.com>
2023-06-07 11:24:15 +0200

    Add iOSSwiftUIViewControllerFactory tests

---
+++ ArtGalleryTests/Main/iOSSwiftUIViewControllerFactoryTest.swift
@@ -0,0 +1,31 @@
+//
+//  iOSSwiftUIViewControllerFactoryTest.swift
+//  ArtGalleryTests
+//
+//  Created by nikolamilic on 6/7/23.
+//
+
+import XCTest
+import SwiftUI
+@testable import ArtGallery
+
+final class iOSSwiftUIViewControllerFactoryTest: XCTestCase {
+ 
+    func test_searchViewController_createsControllerWithArtworkBriefSearchViewRootView() {
+        let sut = iOSSwiftUIViewControllerFactory()
+        let controller = sut.searchViewController(briefsLoadedAction: { _,_ in }) as? UIHostingController<ArtworkBriefSearchView>
+        
+        XCTAssertNotNil(controller)
+    }
+    
+    func test_listViewController_createsControllerWithArtworkBriefListViewRootView() {
+        let sut = iOSSwiftUIViewControllerFactory()
+        let query = "query"
+        let briefs = ArtworkBrief.dummyData
+        let controller = sut.briefListViewController(searchQuery: query, briefs: briefs, briefTapAction: { _ in}) as? UIHostingController<ArtworkBriefListView>
+        
+        XCTAssertNotNil(controller)
+        XCTAssertEqual(controller?.rootView.viewModel.briefs, briefs)
+        XCTAssertEqual(controller?.rootView.viewModel.searchQuery, query)
+    }
+}
